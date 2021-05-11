81f3ad7f34b4643989dbf61a8003144609c99c0a
Alexandros Zachariades <alexandros@office.belugga.com>
2021-05-11 22:19:52 +0300

    moved constants

--- Constants.cs
+++ Constants.cs
@@ -4,5 +4,10 @@
     {
         //windows message id for hotkey
         public const int WM_HOTKEY_MSG_ID = 0x0312;
+
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+        public const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        public const int MOUSEEVENTF_RIGHTUP = 0x10;
     }
 }
